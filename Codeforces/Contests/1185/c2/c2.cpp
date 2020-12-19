6ace506bc9c2684efd8c727c3bf2f0f60471b82d
Manav Kushwaha <manavkush@gmail.com>
2020-12-19 16:51:13 +0530

    Gave 2 rounds on Codeforces

---
+++ Codeforces/Contests/1185/c2/c2.cpp
@@ -0,0 +1,115 @@
+// Author: manavkush
+// Date: 18/12/2020
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define int long long
+typedef vector<int> vi;
+#define all(x) x.begin(), x.end()
+#define FIO     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define re(i,n)        for(int i=0;i<(n);i++)
+#define re1(i, k, n) for (int i = k; k < n ? i <= n : i >= n; k < n ? i += 1 : i -= 1)
+#define FORD(i,a,b)     for(int i=(a);i>=(b);i--)
+typedef pair<int, int> pii;
+typedef priority_queue<pii, vector<pii>, greater<pii>> minpq;
+typedef priority_queue<pii> maxpq;
+const int mod = 1000000007;
+//===================================DEBUG TEMPLATE=================================================
+void __print(int x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+ 
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+//====================================DEBUG TEMPLATE==============================================
+
+
+int32_t main()
+{
+    FIO;
+    // int t;cin>>t;
+    // while(t--)
+    // {
+        
+    // }
+    int n,M;
+    cin>>n>>M;
+    vi a(n);
+    set<int> s;
+    int sum = 0;
+    int sum2=0;
+    int count=0;
+    vi res(n,0);
+    // map <int,int, greater<int> > hash;
+    multiset<int,greater<int> > hash;
+    re(i,n) {
+        cin>>a[i];
+        if(sum-sum2+a[i]<=M) {
+            res[i]=count;
+        } 
+        else {
+            auto itr=hash.begin();
+            
+            count=0;
+            sum2=0;
+            while(sum-sum2+a[i]>M) {
+                sum2+=(*itr);
+                itr++;
+                count++;
+            }
+            res[i]=count;
+        }
+        debug(res);
+        hash.insert(a[i]);
+        sum+=a[i];
+    }
+
+    for(auto x: res) {
+        cout<<x<<" ";
+    }
+}
