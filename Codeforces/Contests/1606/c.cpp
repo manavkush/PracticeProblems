ce713ed43aac46175a2137980846163cdde88f20
Manav Kushwaha <manavkush@gmail.com>
2021-11-11 10:45:29 +0530

    cf 1607

---
+++ Codeforces/Contests/1606/c.cpp
@@ -0,0 +1,121 @@
+// Author: $%U%$
+// Date: $%D%$/$%M%$/$%Y%$
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define int long long
+typedef vector<int> vi;
+#define all(x) x.begin(), x.end()
+#define FIO     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define re(i,n)        for(int i=0;i<(n);i++)
+#define re1(i, k, n) for (int i = k; k < n ? i <= n : i >= n; k < n ? i += 1 : i -= 1)
+#define FORD(i,a,b)     for(int i=(a);i>=(b);i--)
+typedef pair<int, int> pii;
+typedef priority_queue<pii, vector<pii>, greater<pii>> minpq;
+typedef priority_queue<pii> maxpq;
+const int mod = 1000000007;
+//===================================DEBUG TEMPLATE=================================================
+void __print(int x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+ 
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+//====================================DEBUG TEMPLATE==============================================
+void solve() {
+    int n,k;
+    cin>>n>>k;
+
+    vi a(n);
+    re(i,n) {
+        cin>>a[i];
+        a[i] = pow(10, a[i]);
+    }
+    vi b(n,0);
+    int curr = 0;
+    int idx = 0;
+    while(1) {
+        int rem = k-curr;
+        if(idx==n-1) {
+            b[idx] += rem+1;
+            // debug(rem);
+            break;
+        } else {
+            int allowed = a[idx+1]/a[idx]-1;
+            // debug(rem, allowed);
+            if(allowed<rem) {
+                b[idx] = allowed;
+                curr += allowed;
+                idx++;
+            } else if(allowed==rem) {
+                b[idx] = rem;
+                b[idx+1] = 1; 
+                break;
+            } else {
+                b[idx] = rem+1;
+                break;
+            }
+        }
+    }
+    int ans = 0;
+    for(int i=0;i<n ;i++) {
+        ans += b[i]*a[i];
+    }
+    cout<<ans<<endl;
+
+}
+int32_t main()
+{
+    FIO;
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
